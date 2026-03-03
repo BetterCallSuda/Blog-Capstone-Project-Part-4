df57472736d3a1ff77296aafc968b5a0869892e2
Sudharson Saravanan <160273862+BetterCallSuda@users.noreply.github.com>
2026-03-03 22:27:18 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from datetime import date
+from flask import Flask, abort, render_template, redirect, url_for, flash, request
+from flask_bootstrap import Bootstrap5
+from flask_ckeditor import CKEditor
+from flask_gravatar import Gravatar
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Text
+from functools import wraps
+from werkzeug.security import generate_password_hash, check_password_hash
+from wtforms import SubmitField, StringField
+from wtforms.fields.simple import PasswordField
+
+# Import your forms from the forms.py
+from forms import CreatePostForm, LoginForm
+
