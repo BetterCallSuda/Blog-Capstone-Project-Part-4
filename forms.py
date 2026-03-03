67945c9347df8be6f27ff57e1fcd98bbd1a9409e
Sudharson Saravanan <160273862+BetterCallSuda@users.noreply.github.com>
2026-03-03 22:38:01 +0530

    Update forms.py

--- forms.py
+++ forms.py
@@ -13,7 +13,14 @@ class CreatePostForm(FlaskForm):
     body = CKEditorField("Blog Content", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
 
+# TODO: Create a RegisterForm to register new users
+class LoginForm(FlaskForm):
+    email = StringField("Email")
+    password = PasswordField("Password")
+    submit = SubmitField("Login")
 
+# TODO: Create a LoginForm to login existing users
 
 
 # TODO: Create a CommentForm so users can leave comments below posts
+
